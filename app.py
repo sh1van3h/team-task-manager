1f85d7c9941536f01e15e2ba3b9899fb8e992042
Sh1van3h <spparmar234234@gmail.com>
2026-05-22 17:35:21 +0530

    Fixed Railway dynamic port

--- app.py
+++ app.py
@@ -309,5 +309,5 @@ def add_member(project_id):
     return render_template("add_member.html")
     
 if __name__ == "__main__":
-    port = int(os.environ.get("POST",5000))
+    port = int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
